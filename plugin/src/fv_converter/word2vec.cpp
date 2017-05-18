3bd47a92f1dcb3abb732f3150e0dd8011d4ff754
Tetsuya Shioda <shioda.tetsuya@lab.ntt.co.jp>
2017-05-18 09:44:25 +0900

    add word2vec plugin template

---
+++ plugin/src/fv_converter/word2vec.cpp
@@ -0,0 +1,37 @@
+// Jubatus: Online machine learning framework for distributed environment
+// Copyright (C) 2011 Preferred Networks and Nippon Telegraph and Telephone Corporation.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License version 2.1 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include "word2vec.hpp"
+
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "jubatus/util/data/string/utility.h"
+#include "jubatus/core/fv_converter/exception.hpp"
+#include "jubatus/core/fv_converter/util.hpp"
+
+namespace jubatus {
+namespace plugin {
+namespace fv_converter {
+
+}  // namespace fv_converter
+}  // namespace plugin
+}  // namespace jubatus
+
+extern "C" {
+}  // extern "C"
---
+++ plugin/src/fv_converter/word2vec_test.cpp
@@ -0,0 +1,34 @@
+// Jubatus: Online machine learning framework for distributed environment
+// Copyright (C) 2011 Preferred Networks and Nippon Telegraph and Telephone Corporation.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License version 2.1 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include <gtest/gtest.h>
+#include "jubatus/util/concurrent/thread.h"
+#include "jubatus/util/lang/bind.h"
+#include "jubatus/util/lang/scoped_ptr.h"
+#include "jubatus/core/fv_converter/exception.hpp"
+#include "word2vec.hpp"
+
+namespace jubatus {
+namespace plugin {
+namespace fv_converter {
+
+}  // namespace fv_converter
+}  // namespace plugin
+}  // namespace jubatus
